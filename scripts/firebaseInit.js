70064c35ecdc2a4356ead045177d68f93539e046
Yralec <hipigmaster@gmail.com>
2019-05-09 17:41:43 +0900

    firebase init and login html

---
+++ scripts/firebaseInit.js
@@ -0,0 +1,11 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4gYItaWV0Qilxl3k8CBwgjpIdKQs_WG8",
+    authDomain: "lettucegoeat.firebaseapp.com",
+    databaseURL: "https://lettucegoeat.firebaseio.com/",
+    projectId: "lettucegoeat",
+    storageBucket: "lettucegoeat.appspot.com",
+    messagingSenderId: "491280351750",
+    appId: "1:491280351750:web:2d45c5065ab4ecdb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
