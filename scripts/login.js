30c827006bef5d5775c2ba08254512a9dc575257
Yralec <hipigmaster@gmail.com>
2019-05-09 18:25:41 +0900

    login js

---
+++ scripts/login.js
@@ -0,0 +1,61 @@
+const failReason = {
+	USERNAME: 0,
+	PASSWORD: 1,
+	INCORRECT: 2
+}
+
+
+$(document).ready(function() {
+
+	$("#login").click(() => {
+		var username = $("input[name='username']").val()
+		var password = $("input[name='password']").val()
+
+		if (username.length == 0) {
+			failLogin(failReason.USERNAME)
+		} else if (password.length == 0) {
+			failLogin(failReason.PASSWORD)
+		} else {
+
+			firebase.database().ref('/users/').orderByChild('username').equalTo(username).once("value", (snapshot) => {
+				var obj = snapshot.val()
+				//not secure but this isn't the point of this project
+				var key = Object.keys(obj)[0]
+				if (obj[key].password == password) {
+					login(username)
+				} else {
+					failLogin(failReason.INCORRECT)
+				}
+			})
+		}
+	})
+
+});
+
+function failLogin(reason) {
+	switch (reason) {
+		case failReason.USERNAME:
+			console.log("username");
+			break;
+		case failReason.PASSWORD:
+			console.log("password");
+			break;
+		case failReason.INCORRECT:
+			console.log("incorrect");
+			break;
+		default:
+			console("loginFail");
+			break;
+	}
+}
+
+function login(username) {
+
+	//add cookie
+	var cookie = "path=/;" + "username=" + username
+	console.log(cookie)
+	document.cookie = cookie;
+
+	//change to first page (schedule page)
+	window.location.href = "schedule.html";
+}
