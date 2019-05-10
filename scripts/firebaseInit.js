627ed62869cef6507e085fcfe2b3b5126aff44e1
Yralec <hipigmaster@gmail.com>
2019-05-10 21:36:42 +0900

    tried to make a header

--- scripts/firebaseInit.js
+++ scripts/firebaseInit.js
@@ -1,30 +1,38 @@
-  var firebaseConfig = {
-    apiKey: "AIzaSyC4gYItaWV0Qilxl3k8CBwgjpIdKQs_WG8",
-    authDomain: "lettucegoeat.firebaseapp.com",
-    databaseURL: "https://lettucegoeat.firebaseio.com/",
-    projectId: "lettucegoeat",
-    storageBucket: "lettucegoeat.appspot.com",
-    messagingSenderId: "491280351750",
-    appId: "1:491280351750:web:2d45c5065ab4ecdb"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+$("#header").load("../header.html");
 
-  const failReason = {
-    USERNAME: 0,
-    PASSWORD: 1,
-    INCORRECT: 2
+var firebaseConfig = {
+  apiKey: "AIzaSyC4gYItaWV0Qilxl3k8CBwgjpIdKQs_WG8",
+  authDomain: "lettucegoeat.firebaseapp.com",
+  databaseURL: "https://lettucegoeat.firebaseio.com/",
+  projectId: "lettucegoeat",
+  storageBucket: "lettucegoeat.appspot.com",
+  messagingSenderId: "491280351750",
+  appId: "1:491280351750:web:2d45c5065ab4ecdb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const failReason = {
+  USERNAME: 0,
+  PASSWORD: 1,
+  INCORRECT: 2
+}
+
+function animateCSS(element, animationName, callback) {
+  var el = $(element).addClass('animated ' + animationName)
+
+  function handleAnimationEnd() {
+    el.removeClass('animated ' + animationName)
+    el.off('animationend')
+
+    if (typeof callback === 'function') callback()
   }
 
-  function animateCSS(element, animationName, callback) {
-    var el = $(element).addClass('animated ' + animationName)
-
-    function handleAnimationEnd() {
-        el.removeClass('animated ' + animationName)
-        el.off('animationend')
-
-        if (typeof callback === 'function') callback()
-    }
-
-    el.on('animationend', handleAnimationEnd)
+  el.on('animationend', handleAnimationEnd)
 }
+
+$(document).ready(function() {
+
+  $("header").load("header.html");
+
+});
