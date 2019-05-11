5253f19d72df448d7f5b1194f9b6332bf5e81993
us0310306 <us0310306@kaist.ac.kr>
2019-05-11 15:36:35 +0900

    findgroup

--- findgroup.js
+++ findgroup.js
@@ -1,15 +1,5 @@
 // JavaScript source code
 
-var firebaseConfig = {
-    apiKey: "AIzaSyC4gYItaWV0Qilxl3k8CBwgjpIdKQs_WG8",
-    authDomain: "lettucegoeat.firebaseapp.com",
-    databaseURL: "https://lettucegoeat.firebaseio.com/",
-    projectId: "lettucegoeat",
-    storageBucket: "lettucegoeat.appspot.com",
-    messagingSenderId: "491280351750",
-    appId: "1:491280351750:web:2d45c5065ab4ecdb"
-};
-firebase.initializeApp(firebaseConfig);
 
 firebase.database().ref('groups').orderByChild('/day').once("value", function(datasnapshot){
 	var x = datasnapshot.val();
