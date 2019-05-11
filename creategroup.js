d62d10455e48e0b4424edbbed4cddb84c3ef085d
us0310306 <us0310306@kaist.ac.kr>
2019-05-11 14:27:05 +0900

    Update creategroup.js

--- creategroup.js
+++ creategroup.js
@@ -1,14 +1,5 @@
 // JavaScript source code
-var firebaseConfig = {
-    apiKey: "AIzaSyC4gYItaWV0Qilxl3k8CBwgjpIdKQs_WG8",
-    authDomain: "lettucegoeat.firebaseapp.com",
-    databaseURL: "https://lettucegoeat.firebaseio.com/",
-    projectId: "lettucegoeat",
-    storageBucket: "lettucegoeat.appspot.com",
-    messagingSenderId: "491280351750",
-    appId: "1:491280351750:web:2d45c5065ab4ecdb"
-};
-firebase.initializeApp(firebaseConfig);
+
 var sun = document.getElementById('sun')
 var mon = document.getElementById('mon')
 var tue = document.getElementById('tue')
